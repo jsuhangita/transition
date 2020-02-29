94d0de864c56220a4f3183afef1d9d15a4cd1018
Jonathan Suhangita <jonathan.suhangita@dailyact.com>
2020-02-29 11:08:57 +0700

    add component index

--- app/components/base/index.js
+++ app/components/base/index.js
@@ -1,22 +1,15 @@
 import ActionMenu from './ActionMenu/ActionMenu.component';
-import Alert from './Alert/Alert.component';
 import Button from './Button/Button.component';
 import Card from './Card/Card.component';
-import CheckBox from './CheckBox/CheckBox.component';
 import Container from './Container/Container.component';
 import Content from './Content/Content.component';
-import DatePicker from './DatePicker/DatePicker.component';
 import ErrorTextIndicator from './ErrorTextIndicator/ErrorTextIndicator.component';
-import Icon from './Icon/Icon.component';
 import Image from './Image/Image.component';
-import ImagePicker from './ImagePicker/ImagePicker.component';
 import ImageCheckBox from './ImageCheckBox/ImageCheckBox.component';
 import Input from './Input/Input.component';
 import Header from './Header/Header.component';
-import Label from './Label/Label.component';
 import Loading from './Loading/Loading.component';
 import LoadingImage from './LoadingImage/LoadingImage.component';
-import Menu from './Menu/Menu.component';
 import Modal from './Modal/Modal.component';
 import OverlaySpinner from './OverlaySpinner/OverlaySpinner.component';
 import Picker from './Picker/Picker.component';
@@ -24,28 +17,20 @@ import RadioButton from './RadioButton/RadioButton.component';
 import Title from './Title/Title.component';
 import Toggle from './Toggle/Toggle.component';
 import Touchable from './Touchable/Touchable.component';
-import Video from './Video/Video.component';
 
 export {
   ActionMenu,
-  Alert,
   Button,
   Card,
-  CheckBox,
   Content,
   Container,
-  DatePicker,
   ErrorTextIndicator,
-  Icon,
   Image,
   ImageCheckBox,
-  ImagePicker,
   Input,
   Header,
-  Label,
   Loading,
   LoadingImage,
-  Menu,
   Modal,
   OverlaySpinner,
   Picker,
@@ -53,5 +38,4 @@ export {
   Title,
   Toggle,
   Touchable,
-  Video,
 };
